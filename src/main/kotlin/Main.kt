e91bad574a9fbe5aa5c2c5353cc90eaa980c30d5
anastasianadelshtehel <120086013+anastasianadelshtehel@users.noreply.github.com>
2022-12-13 20:54:07 +0300

    delete

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 import java.time.LocalDateTime
 
 fun main(args: Array<String>) {
-    println(LocalDateTime.now())
+    
     println("Hello")
-    println("Program arguments: ${args.joinToString()}")
 
     class abc(){
         var f = 5
+        
     }
 }
